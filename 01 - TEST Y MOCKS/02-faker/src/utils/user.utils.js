7e5fd7389a7ac1da1993c5891da0b64e35350a54
MauriDiPietro <dipietro.jm@gmail.com>
2024-09-23 22:46:33 -0300

    clase 08

--- 01 - TEST Y MOCKS/02-faker/src/utils/user.utils.js
+++ 01 - TEST Y MOCKS/02-faker/src/utils/user.utils.js
@@ -1,6 +1,10 @@
 import { faker } from "@faker-js/faker";
 faker.locale = "es";
 
+/**
+ * funcion que retorna un objeto usuario desde faker
+ * @returns usuario faker
+ */
 export const generateUser = () => {
   return {
     name: faker.name.findName(),
